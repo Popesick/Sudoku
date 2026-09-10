4203f853a6d7943331ef66f2fcfcfd5cbac4270d
Popesick <andreas.stetter@telefonica.com>
2026-09-10 15:16:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sudoku-cache-v3';
+const CACHE_NAME = 'sudoku-cache-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png', './icon-512-maskable.png', './hirsch.png'];
 
 self.addEventListener('install', (event) => {
